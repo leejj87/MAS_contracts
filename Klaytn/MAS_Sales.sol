b9f143be38f11ba5a0ffdf0fb292fb3999745403
leejj87 <58094579+leejj87@users.noreply.github.com>
2021-12-28 20:05:40 +0900

    Update MAS_Sales.sol

--- Klaytn/MAS_Sales.sol
+++ Klaytn/MAS_Sales.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.5.0;
 import "./factory.sol";
 import "./bank.sol";
+import "./whiteLists.sol";
 contract MAS_Sales is Ownable,ReentrancyGuard,Pausable{
     using SafeMath for uint256;
     MAS private nftAddress;
     address private bankAddress;
     bank private bankProxy;
+    WhiteLists private whiteListsAddress;
     mapping(uint256 => uint256) SalesPrice;
     mapping(uint256 => address) RegisteredSeller;
     event logPurchase(uint256 indexed _tokenId,
@@ -13,19 +15,25 @@ contract MAS_Sales is Ownable,ReentrancyGuard,Pausable{
                     uint256 indexed _buy_price
                     );
     constructor(address _nftAddress,
-                address payable _bankAddress) public {
+                address payable _bankAddress,
+                address _whiteLIstsAddress) public {
         nftAddress = MAS(_nftAddress);
         bankAddress = _bankAddress;
         bankProxy = bank(_bankAddress);
+        whiteListsAddress = WhiteLists(_whiteLIstsAddress);
     }
-    function setSales(address _to,uint256 _tokenId, uint256 _price) public onlyOwner whenNotPaused{
+    modifier whiteUsersOnly(){
+        require(whiteListsAddress.getWhiteLists(_msgSender())==true,"operators in the whitelist only");
+        _;
+    }
+    function setSales(address _to,uint256 _tokenId, uint256 _price) public whiteUsersOnly whenNotPaused{
         require(nftAddress.ownerOf(_tokenId)==_to,"not a token owner nor contract owner");
         //nftAddress.setApprovalForAll(address(this),true);
         require(nftAddress.isApprovedForAll(_to,address(this)),"need permission to the address");
         SalesPrice[_tokenId]=_price;
         RegisteredSeller[_tokenId]=_to;
     }
-    function removeSales(address _to, uint256 _tokenId) public onlyOwner{
+    function removeSales(address _to, uint256 _tokenId) public whiteUsersOnly{
         require(_to != address(0),"address never be 0");
         require(nftAddress.ownerOf(_tokenId)==_to,"not a token owner nor contract owner");
         SalesPrice[_tokenId]=0;
@@ -48,5 +56,18 @@ contract MAS_Sales is Ownable,ReentrancyGuard,Pausable{
         RegisteredSeller[_tokenId]=address(0);
         emit logPurchase(_tokenId,msg.sender,msg.value);
         return bankProxy.deposit(_tokenId,msg.sender,_seller,msg.value);
+    }
+    function pause() public whiteUsersOnly {
+        pause();
+    }
+    /**
+     * @dev Returns to normal state.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    function unpause() public whiteUsersOnly{
+        unpause();
     }
 }
