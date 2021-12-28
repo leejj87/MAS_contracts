d59322c9e1e55af76c941800b6ef6535fb2ba6b6
leejj87 <58094579+leejj87@users.noreply.github.com>
2021-12-28 16:13:45 +0900

    Create whiteLists.sol

---
+++ polygon/whiteLists.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+
+contract WhiteLists is Ownable, Pausable {
+
+    mapping(address=>bool) white_lists;
+    constructor(){
+        setWhiteLists(msg.sender,true);
+    }
+    function setWhiteLists(address _address, bool _allow) public onlyOwner {
+        white_lists[_address]=_allow;
+    }
+
+    function getWhiteLists(address _address) public view returns(bool){
+        return white_lists[_address];
+    }
+
+
+
+
+}
