92f0c89821c74dd52ab21c6d8fee12524ad72fa9
leejj87 <58094579+leejj87@users.noreply.github.com>
2021-12-30 15:46:56 +0900

    Update whiteLists.sol

--- Klaytn/whiteLists.sol
+++ Klaytn/whiteLists.sol
@@ -241,6 +241,7 @@ contract Pausable is PauserRole {
 contract WhiteLists is Ownable, Pausable {
 
     mapping(uint256=>mapping(address=>bool)) white_lists;
+    address _operator;
     constructor() public {
         setWhiteLists(0,msg.sender,true);
         setWhiteLists(1,msg.sender,true);
@@ -254,6 +255,13 @@ contract WhiteLists is Ownable, Pausable {
     function getWhiteLists(uint256 _type,address _address) public view returns(bool){
         return white_lists[_type][_address];
     }
+    function setOperator(address operator) public onlyOwner{
+        _operator=operator;
+    }
+    function getOperator() public view returns(address){
+        return _operator;
+    }
+
     function pause() public onlyOwner {
         pause();
     }
