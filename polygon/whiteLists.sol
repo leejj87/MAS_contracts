93936f5f9d6aaea35fa6dc3aba79e39fd262773c
leejj87 <58094579+leejj87@users.noreply.github.com>
2021-12-30 14:26:38 +0900

    Update whiteLists.sol

--- polygon/whiteLists.sol
+++ polygon/whiteLists.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 contract WhiteLists is Ownable, Pausable {
     //0:minting, 1:royalty 2:bank 3:purchase
     mapping(uint256=>mapping(address=>bool)) white_lists;
+    address _operator;
     constructor(){
         setWhiteLists(0,msg.sender,true);
         setWhiteLists(1,msg.sender,true);
@@ -19,6 +20,12 @@ contract WhiteLists is Ownable, Pausable {
 
     function getWhiteLists(uint256 _type,address _address) public view returns(bool){
         return white_lists[_type][_address];
+    }
+    function setOperator(address operator) public onlyOwner{
+        _operator=operator;
+    }
+    function getOperator() public view returns(address){
+        return _operator;
     }
 
     function pause() public onlyOwner {
